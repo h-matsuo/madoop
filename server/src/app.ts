e22374479aabb522f8ebb6b31a614d109cb10392
Hiroyuki Matsuo <h-matsuo@ist.osaka-u.ac.jp>
2018-04-09 18:27:20 +0900

    [update] C++ sample code

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,10 +9,13 @@ let job = new Job();
 let map = new Map();
 let reduce = new Reduce();
 
-let source = `
+let source: string = null;
+
+///// Example of pure JavaScript ///////////////////////////////////////////////
+source = `
 /*
-* Madoop implementation example: word count: map function
-*/
+ * Madoop implementation example: word count: map function
+ */
 function map( data /* :string   */ ,
               emit /* :function */ ) {
   // Initialize result
@@ -36,21 +39,59 @@ map.setJs(new Function('data', 'emit', `
   ${source}
   map(data, emit);
 `));
+////////////////////////////////////////////////////////////////////////////////
 
-
+///// Example of C//////////////////////////////////////////////////////////////
+source = `
 /*
-map.setLanguage(Language.Cpp);
-map.setSource(`
-  #include <iostream>
-  extern "C" {
-    void code()
-    {
-      std::cout << "Hello, world!" << std::endl;
+ * Madoop implementation example: word count: map function
+ */
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+
+std::vector<std::string> split(const std::string &, char);
+
+extern "C" { // REQUIRED to prevent C++ name mangling
+
+  extern void emit(const char*, const char*); // REQUIRED to emit key-value pairs
+
+  void map(const char* data)
+  {
+    // Count the number of occurrences of each word
+    const std::string &data_str = std::string(data);
+    std::unordered_map<std::string, int> hashmap;
+    const std::vector<std::string> &words = split(data_str, ' ');
+    for (auto &element: words) {
+      hashmap[element]++;
+    }
+    // Emit key-value pairs
+    for (auto &element: hashmap) {
+      const std::string &key   = element.first;
+      const std::string &value = std::to_string(element.second);
+      emit(key.c_str(), value.c_str());
     }
   }
-`);
+
+}
+
+// Split \`std::string\` with \`char\`
+std::vector<std::string> split(const std::string &str, char sep)
+{
+  std::vector<std::string> v;
+  std::stringstream ss(str);
+  std::string buffer;
+  while(std::getline(ss, buffer, sep)) {
+    v.push_back(buffer);
+  }
+  return v;
+}
+`;
+map.setLanguage(Language.Cpp);
+map.setSource(source);
 map.compile();
-*/
+////////////////////////////////////////////////////////////////////////////////
 
 job.setMap(map);
 
