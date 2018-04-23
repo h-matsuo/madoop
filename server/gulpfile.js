5f0e79cbaaf192909b40d71f702fdf2c1e69437d
Hiroyuki Matsuo <h-matsuo@ist.osaka-u.ac.jp>
2018-04-23 15:58:14 +0900

    [update] refs #1 minify JS code
    Resolves #1.

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -3,6 +3,8 @@ const ts         = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
 const plumber    = require('gulp-plumber'); // エラー時に Watch を停止させない
 const notify     = require('gulp-notify');  // エラー時に通知を出す
+const uglify     = require('gulp-uglifyes');// JS minifier (compatible with ES6)
+const rename     = require('gulp-rename');
 
 gulp.task('build', () => {
   const project = ts.createProject('./tsconfig.json');
@@ -22,4 +24,20 @@ gulp.task('build', () => {
 
 gulp.task('build:watch', ['build'], () => {
   return gulp.watch('./src/**/*.ts', ['build']);
+});
+
+gulp.task('minify', () => {
+  return gulp.src([
+    './build/**/*.js',
+    '!./build/**/*.min.js'
+  ])
+  .pipe(plumber({
+    errorHandler: notify.onError('Error: <%= error.message %>')
+  }))
+  .pipe(uglify())
+  .on('error', function(e){
+    console.error(e);
+  })
+  .pipe(rename({extname: '.min.js'}))
+  .pipe(gulp.dest('./build/'));
 });
