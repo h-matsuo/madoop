5ae3858117675ef0f2d8ee802d63dc2a00569436
Hiroyuki Matsuo <h-matsuo@ist.osaka-u.ac.jp>
2018-04-27 00:44:25 +0900

    [update] aggregate web server feature into Server.ts

---
+++ server/src/webapi-extension/Server.ts
@@ -0,0 +1,112 @@
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors'; // Cross-Origin Resource Sharing
+import * as express from 'express';
+import * as log4js from 'log4js';
+
+// import DataBase from '../lib/DataBase';
+import Job from '../lib/Job';
+import Task from '../lib/Task';
+import MadoopError from '../lib/MadoopError';
+
+export default
+class Server {
+
+  private app: express.Express;
+  private router: express.Router;
+  private logger: log4js.Logger;
+
+  private job: Job;
+
+  private DOMAIN: string = process.env.MADOOP_DOMAIN || 'localhost';
+  private PORT: string   = process.env.MADOOP_PORT   || '3000';
+  private ROOT: string   = process.env.MADOOP_ROOT   || '/madoop';
+
+  constructor(job?: Job) {
+    this.app = express();
+    this.router = express.Router();
+    this.logger = log4js.getLogger();
+    this.logger.level = 'debug';
+    if (job) {
+      this.job = job;
+    }
+  }
+
+  private printLog(msg: string): void {
+    this.logger.info(msg);
+  }
+
+  private convertMapToObject(map: Map<any, any[]>): { key: any, values: any[] }[] {
+    const result: { key: any, values: any[] }[] = [];
+    map.forEach((values, key) => {
+      const element = {
+        key: key,
+        values: values
+      };
+      result.push(element);
+    });
+    return result;
+  }
+
+  setJob(job: Job): void {
+    this.job = job;
+  }
+
+  run(): void {
+
+    this.router.get('/tasks', (req, res): void => {
+      this.printLog(`[GET] /tasks - from ${req.hostname} (${req.ip})`);
+      res.sendStatus(500);
+    });
+
+    this.router.get('/tasks/next', (req, res): void => {
+      this.printLog(`[GET] /tasks/next - from ${req.hostname} (${req.ip})`);
+      let taskInfo = {
+        taskId: <string> null,
+        inputData: <any> null,
+        funcString: <string> null
+      };
+      const task = this.job.getNextTask();
+      if (!task) { res.send(taskInfo); return; }
+
+      taskInfo.taskId = task.getTaskId();
+      taskInfo.inputData = task.getTaskInputData();
+      if (taskInfo.taskId === 'map') {
+        taskInfo.funcString = this.job.getMapper().map.toString();
+      } else if (taskInfo.taskId === 'reduce') {
+        taskInfo.funcString = this.job.getReducer().reduce.toString();
+        const inputDataObject = this.convertMapToObject(taskInfo.inputData);
+        taskInfo.inputData = JSON.stringify(inputDataObject);
+      } else {
+        throw new MadoopError(`unknown task id: ${taskInfo.taskId}`);
+      }
+      res.send(taskInfo);
+    });
+
+    this.router.post('/tasks/result', (req, res): void => {
+      this.printLog(`[POST] /tasks/result - from ${req.hostname} (${req.ip})`);
+      const task = new Task();
+      task.setTaskId(req.body.taskId);
+      task.setResult(JSON.parse(req.body.result));
+      this.job.completeTask(task);
+      res.sendStatus(201);
+    });
+
+
+    // Settings for CORS: Cross-Origin Resource Sharing
+    this.app.use(cors());
+
+    // Settings for parsing JSON as request body
+    this.app.use(bodyParser.urlencoded({
+      limit: '100mb', // to avoid `PayloadTooLargeError`
+      extended: true
+    }));
+    this.app.use(bodyParser.json());
+
+    this.app.use(this.ROOT, this.router);
+    this.app.use(this.ROOT, express.static('public'));
+    this.app.listen(this.PORT);
+    this.printLog(`listen on port ${this.PORT}`);
+
+  }
+
+}
--- server/src/webapi-extension/rain/app.ts
+++ server/src/webapi-extension/rain/app.ts
@@ -1,47 +1,11 @@
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors'; // Cross-Origin Resource Sharing
-import * as express from 'express';
-import * as log4js from 'log4js';
-
-// import DataBase from '../../lib/DataBase';
-import Job from '../../lib/Job';
-import Task from '../../lib/Task';
-
-declare const ajaxPostJson: (url: string, jsonData: Object) => Promise<string>;
-
-const app = express();
-const router = express.Router();
-// const db = new DataBase();
-const logger = log4js.getLogger();
-logger.level = 'debug';
-
-const DOMAIN = process.env.MADOOP_DOMAIN || 'localhost';
-const PORT   = process.env.MADOOP_PORT   || 3000;
-const ROOT   = process.env.MADOOP_ROOT   || '/madoop';
-
-const printLog = (msg: string): void => {
-  logger.info(msg);
-}
-
-const convertMapToObject = (map: Map<any, any[]>): { key: any, values: any[] }[] => {
-  const result: { key: any, values: any[] }[] = [];
-  map.forEach((values, key) => {
-    const element = {
-      key: key,
-      values: values
-    };
-    result.push(element);
-  });
-  return result;
-};
-
-
-///// Job Settings /////////////////////////////////////////////////////////////
 import * as fs from 'fs';
+
+import Job from '../../lib/Job';
+import Server from '../Server';
 import InputData from '../../lib/InputData';
 import MyMapper from '../../rain/MyMapper';
 import MyReducer from '../../rain/MyReducer';
-import MadoopError from '../../lib/MadoopError';
+
 class MyInputData extends InputData {
   constructor() {
     super();
@@ -58,6 +22,7 @@ class MyInputData extends InputData {
     this.addInputData(data.slice(step * (length % step)));
   }
 }
+
 const job = new Job('rain');
 const inputData = new MyInputData();
 const mapper = new MyMapper();
@@ -65,59 +30,6 @@ const reducer = new MyReducer();
 job.setInputData(inputData);
 job.setMapper(mapper);
 job.setReducer(reducer);
-////////////////////////////////////////////////////////////////////////////////
 
-
-router.get('/tasks', (req, res): void => {
-  printLog(`[GET] /tasks - from ${req.hostname} (${req.ip})`);
-  res.sendStatus(500);
-});
-
-router.get('/tasks/next', (req, res): void => {
-  printLog(`[GET] /tasks/next - from ${req.hostname} (${req.ip})`);
-  let taskInfo = {
-    taskId: <string> null,
-    inputData: <any> null,
-    funcString: <string> null
-  };
-  const task = job.getNextTask();
-  if (!task) { res.send(taskInfo); return; }
-
-  taskInfo.taskId = task.getTaskId();
-  taskInfo.inputData = task.getTaskInputData();
-  if (taskInfo.taskId === 'map') {
-    taskInfo.funcString = job.getMapper().map.toString();
-  } else if (taskInfo.taskId === 'reduce') {
-    taskInfo.funcString = job.getReducer().reduce.toString();
-    const inputDataObject = convertMapToObject(taskInfo.inputData);
-    taskInfo.inputData = JSON.stringify(inputDataObject);
-  } else {
-    throw new MadoopError(`unknown task id: ${taskInfo.taskId}`);
-  }
-  res.send(taskInfo);
-});
-
-router.post('/tasks/result', (req, res): void => {
-  printLog(`[POST] /tasks/result - from ${req.hostname} (${req.ip})`);
-  const task = new Task();
-  task.setTaskId(req.body.taskId);
-  task.setResult(JSON.parse(req.body.result));
-  job.completeTask(task);
-  res.sendStatus(201);
-});
-
-
-// Settings for CORS: Cross-Origin Resource Sharing
-app.use(cors());
-
-// Settings for parsing JSON as request body
-app.use(bodyParser.urlencoded({
-  limit: '100mb', // to avoid `PayloadTooLargeError`
-  extended: true
-}));
-app.use(bodyParser.json());
-
-app.use(ROOT, router);
-app.use(ROOT, express.static('public'));
-app.listen(PORT);
-printLog(`listen on port ${PORT}`);
+const server = new Server(job);
+server.run();
