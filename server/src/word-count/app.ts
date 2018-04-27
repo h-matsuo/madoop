fbf71ae2789927e8fd0a7adc08c733ac645ba8a1
Hiroyuki Matsuo <h-matsuo@ist.osaka-u.ac.jp>
2018-04-27 15:00:43 +0900

    [add] sample of word count

--- server/src/word-count/app.ts
+++ server/src/word-count/app.ts
@@ -3,7 +3,7 @@ import MyInputData from './MyInputData';
 import MyMapper from './MyMapper';
 import MyReducer from './MyReducer';
 
-const job = new Job('words');
+const job = new Job('word-count');
 const inputData = new MyInputData();
 const mapper = new MyMapper();
 const reducer = new MyReducer();
