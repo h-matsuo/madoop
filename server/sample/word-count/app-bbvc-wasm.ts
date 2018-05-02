7635d1ac1cd42a512b3928da03a0dffd7dc07f9f
Hiroyuki Matsuo <h-matsuo@ist.osaka-u.ac.jp>
2018-05-02 17:23:59 +0900

    [update] compatible word-count sample with WebAssembly

---
+++ server/sample/word-count/app-bbvc-wasm.ts
@@ -0,0 +1,33 @@
+import Job from '../../madoop/Job';
+import WasmWebServer from '../../madoop/webapi-extension/WasmWebServer';
+import MyInputData from './MyInputData';
+import WasmMapper from '../../madoop/WasmMapper';
+import WasmReducer from '../../madoop/WasmReducer';
+
+import * as fs from 'fs';
+
+const job = new Job('word-count');
+const inputData = new MyInputData();
+const mapper = new WasmMapper();
+const reducer = new WasmReducer();
+const server = new WasmWebServer();
+
+const wasmJsMap = fs.readFileSync('./map.js', 'utf8');
+const wasmBinaryMap = fs.readFileSync('./map.wasm');
+mapper.setWasmJs(wasmJsMap);
+mapper.setWasmBinary(wasmBinaryMap);
+
+const wasmJsReducer = fs.readFileSync('./reduce.js', 'utf8');
+const wasmBinaryReducer = fs.readFileSync('./reduce.wasm');
+reducer.setWasmJs(wasmJsReducer);
+reducer.setWasmBinary(wasmBinaryReducer);
+
+job.setInputData(inputData);
+job.setMapper(mapper);
+job.setReducer(reducer);
+job.setCallbackWhenCompleted(result => {
+  console.log(result);
+  process.exit(0);
+});
+server.setJob(job);
+server.run();
